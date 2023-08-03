80aa8c60a7b49853a1fa0b863b68cac0cd6995cf
abdullah <abdullah_malik@hotmail.com>
2023-08-03 13:04:23 +0500

    more config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as cors from 'cors'; // Import the CORS middleware
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Enable CORS using the cors middleware
+  app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your Next.js frontend URL
+    // You can also configure other CORS options here if needed
+  }));
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(3001);
 }
 bootstrap();
